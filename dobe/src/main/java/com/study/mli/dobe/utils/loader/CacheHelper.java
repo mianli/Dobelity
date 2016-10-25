a79b6db0fd30f1cff9171fb45a9b5460fef2b05e
limian <crown_li@126.com>
2016-10-25 14:02:23 +0800

    structure

---
+++ dobe/src/main/java/com/study/mli/dobe/utils/loader/CacheHelper.java
@@ -0,0 +1,8 @@
+package com.study.mli.dobe.utils.loader;
+
+/**
+ * Created by limian on 2016/10/25.
+ */
+
+public class CacheHelper {
+}
--- dobe/src/main/java/com/study/mli/dobe/utils/loader/ImageLoader.java
+++ dobe/src/main/java/com/study/mli/dobe/utils/loader/ImageLoader.java
@@ -1,12 +1,14 @@
 package com.study.mli.dobe.utils.loader;
 
 import android.os.Handler;
+import android.util.Log;
 
 import com.study.mli.dobe.app.DBGlobal;
 import com.study.mli.dobe.customview.GifImageView;
 import com.study.mli.dobe.utils.ilview.ILImageView;
 import com.study.mli.dobe.utils.ilview.ILView;
 
+import java.lang.ref.WeakReference;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.concurrent.ExecutorService;
@@ -19,8 +21,11 @@ import java.util.concurrent.Future;
 public class ImageLoader{
 
     private ExecutorService executorService;
+    private Map<String, WeakReference<Future>> mLoadList = new HashMap<>();
     private Handler mHandler;
     public static ImageLoader mInstance;
+
+    private int times = 0;
 
     private Map<String, ILView> mViews = new HashMap<>();
 
@@ -58,7 +63,20 @@ public class ImageLoader{
         final ILView ilView = new ILImageView(imgv);
 
         startLoadView(ilView);
+        if(mLoadList.containsKey(url)) {
+            WeakReference<Future> loader = mLoadList.get(url);
+            Future future = loader.get();
+            if(future != null && !future.cancel(true)) {
+                loadImageByNet(url, imgv, ilView);
+            }else {
+                mLoadList.remove(url);
+            }
+        }else {
+            loadImageByNet(url, imgv, ilView);
+        }
+    }
 
+    private void loadImageByNet(final String url, final GifImageView imgv, final ILView ilView) {
         LoadImage li = creator(mHandler, url, imgv, new iLoadFinishListener() {
             @Override
             public void onFinish(boolean successful, final byte[] bytes) {
@@ -75,13 +93,14 @@ public class ImageLoader{
                             }
 
                             SetImageUtils.getInstance().setImageView(url, imgv, bytes);
+                            Log.i("testtesttest", "" + mLoadList.size());
                         }
                     });
                 }
             }
         });
         Future future = executorService.submit(li);//.execute(li);
-//            m.put(imgv, future);
+        mLoadList.put(url, new WeakReference<>(future));
     }
 
     LoadImage creator(Handler handler, String url, GifImageView imgv, iLoadFinishListener listener) {
